1bb596e28579a66e4f2d2649c140724317091536
Robert <nicoara.robert@gmail.com>
2017-04-09 22:54:02 +0300

    Functional | Arguments implemented

--- rsync_m.py
+++ rsync_m.py
@@ -2,6 +2,16 @@
 import os
 import sys
 from subprocess import Popen, PIPE
+import argparse
+
+par = argparse.ArgumentParser("This script  uses rsync to make a clone from NFS sources you give  \n\
+ex: script -H <NFS SERVER HOST> -m <Temporary mount point> -c <Directory where to clone content> \n \n")
+par.add_argument("-H", help="Source hosname or ip", required=True, dest="H" )
+par.add_argument("-c", help=" Clone Directory", required=False, default="/tmp/clone", dest="C" )
+par.add_argument("-m", help="Mount Directory", required=False, default="/tmp/tempmount", dest="M" )
+
+args = par.parse_args()
+
 
 
 
@@ -42,38 +52,33 @@ def rsync(source, target):
     """
     Make Rsync copy
     """
-    command = "rsync -avh " + source + " " + target
-    #try:
+    command = "rsync -avh " + source + "/*" + " " + target
     cop = bash(command)
-    cop[0]
-    #except OSError:
-     #   print "Can\'t copy"
-      #  sys.exit(1)
-
 
 
 def main(sourceip):
     ip = sourceip
     command = "showmount -e " + ip + " | grep -i reserved"
     source = bash(command)[0].split() # Take source dir
-   
-    print source
+
     if bash(command)[2] == 0:
-        print "Found NFS share"
-        dirs = ["/tmp/newdest", "/tmp/mount"]
+        dirs = [args.C, args.M]
         for direc in dirs:
             create_dir(direc)
-
-        mounter_cmd = "sudo mount -t nfs -o rw,nfsvers=3 " + ip + source[0] + " {}".format(dirs[1])
-        print "Passed the mount"
-        print mounter_cmd
+        mounter_cmd = "sudo mount -t nfs -o rw,nfsvers=3 " + \
+                            ip + ":" + source[0] + " {}".format(dirs[1])
         mounter = bash(mounter_cmd)[2]
-        print mounter
         if mounter == 0:
-            print "Should start rsync but no !"
-            #rsync(dirs[1], dirs[0])
+            rsync(dirs[1], dirs[0])
+            cmd = "sudo umount" + dirs[1]
+            suc = bash(cmd)[2]
+            if suc == 0:
+                try:
+                    os.rmdir(dirs[1])
+                except OSError:
+                    print "Could not delete {}".format(dirs[1])
     else:
-        print "Not found"
+        print "No shared mount point found for this host "
 
 #if __name__ == " __main__":
-main('192.168.1.2')
+main(args.H)
